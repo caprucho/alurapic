c02e6fbecda53543e0a0027bdaf533a222ad4ca5
caprucho <caprucho@gmail.com>
2022-03-19 21:53:55 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+ 
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Começando o curso Vue.js',
+      msg2: 'boraaaaa biirrrrlllll'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
